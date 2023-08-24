bb27d7e2d55e288363e28ee5b29ddfec00d53817
Kaian Mui <kaianmui@gmail.com>
2023-08-24 00:27:11 -0400

    ADDED JAVASCRIPT AND ADDED MORE TIMEBLOCKS IN HTML

--- script.js
+++ script.js
@@ -1,24 +1,63 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+// Sets up the current date and time with JQUERY
+    var currentDate = dayjs();
+    var formattedDate = currentDate.format('dddd, MMMM D, YYYY');
+    $('#currentDay').text(formattedDate);
+
+
+    
+    // updates the time and tells the computer to add or remove classes.
+    function UpdateTime(){
+    var currentHour = dayjs().hour();
+      $('.time-block').each(function(){
+        // A FUNCTION THAT PARSES THE TIME BLOCKS ID'S 
+        var blockHour = parseInt($(this).attr('id').split('-')[1])
+        // REMOVES ALL CLASSES SO EVERYTHING IS RESET TO BEGIN WITH
+        $(this).removeClass('past present future');
+
+        //  ADDS DEPENDING ON TIME.
+        if (blockHour < currentHour){
+          $(this).addClass('past');
+        } else if (blockHour === currentHour){
+          $(this).addClass('present');
+        } else {
+          $(this).addClass('future');
+       
+        }
+
+      })}
+      UpdateTime();
+      // CHECKS TO SEE IF CLASSES NEED TO BE UPDATED EVERY MINUTE
+      setInterval(UpdateTime, 1000);
+
+    $('.saveBtn').on('click', function () {
+      var blockId = $(this).parent().attr('id');
+      var eventText = $(this).siblings('.description').val();
+
+      // Save the event text in local storage with the blockId as the key
+      localStorage.setItem(blockId, eventText);
+      $('#notification').fadeIn().delay(2000).fadeOut(); // Show for 2 seconds
   });
+
+  // Load event data from local storage and populate the text fields
+   $('.time-block').each(function () {
+      var blockId = $(this).attr('id');
+      var eventText = localStorage.getItem(blockId);
+
+      if (eventText) {
+        $(this).find('.description').val(eventText);
+      }
+  });
+// Function to clear local storage and reset the app
+$('#resetButton').on('click', function () {
+  // Clear all data in local storage
+  localStorage.clear();
+
+
+  // Reload the page
+  location.reload();
+  console.log('Reset function has been called.');
+});
+});
+
   
